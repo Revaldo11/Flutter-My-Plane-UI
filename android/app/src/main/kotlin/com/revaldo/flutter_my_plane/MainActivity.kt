fcad6a0a12b0aaaac04d6b11446e520c559facc8
Revaldo11 <revaldoputra11@gmail.com>
2024-06-17 15:07:05 +0700

    setup android FOLFER

--- android/app/src/main/kotlin/com/revaldo/flutter_my_plane/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.revaldo.flutter_my_plane
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
---
+++ android/app/src/main/kotlin/com/revaldo/my_plane/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.revaldo.my_plane
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
